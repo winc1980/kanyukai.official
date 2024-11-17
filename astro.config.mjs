aad8a65ec2f56232167cab26d5425e9f4549ca5a
A1 <tkschr1125@gmail.com>
2024-11-17 17:33:53 +0900

    history

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,5 +15,5 @@ export default defineConfig({
   build: {
     format: "preserve" // NOTE: 出力ファイルの設定
   },
-  integrations: [tailwind()]
+  integrations: [tailwind(), preact()]
 });
