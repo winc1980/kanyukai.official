3d6aa4fa883bcb23ec5bb91df85f3a885b06a4ca
A1 <tkschr1125@gmail.com>
2024-11-20 16:19:54 +0900

    footer carousel background fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,20 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import preact from "@astrojs/preact";
+
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     resolve: {
       alias: {
-        "@": "/src"
-      }
-    }
+        "@": "/src",
+      },
+    },
   },
   build: {
-    format: "preserve" // NOTE: 出力ファイルの設定
+    format: "preserve", // NOTE: 出力ファイルの設定
   },
-  integrations: [tailwind(), preact()]
+  integrations: [tailwind(), preact(), solidJs()],
 });
--- src/components/Period.tsx
+++ src/components/Period.tsx
@@ -1,42 +1,41 @@
-interface periodProps{
-    title: string;
-    year: string;
-    paragraph: string;
-    isDetail: boolean
-    periodClick: () => void;
+interface periodProps {
+  title: string;
+  year: string;
+  paragraph: string;
+  isDetail: boolean;
+  periodClick: () => void;
 }
 
-export default function Period({title, year, isDetail,paragraph, periodClick}: periodProps) {
-
-    return (
-       (isDetail ? (
-            <div className="detail_container">
-                <div className="year">{year}</div>
-                <div className="title">{title}</div>
-                <p>{paragraph}</p>
-                <div className="bg"></div>
-            </div>
-        ): (
-            (Number(year) ? (
-            <>
-                <div class="period_wrapper" onClick={periodClick}>
-                    <span class="left_circle"></span>
-                        <p className="date">{year}</p>
-                        <span class="right_circle"></span>
-                    <p className="title">{title}</p>
-                </div>
-            </>
-            ): (
-                <>
-                <div class="mini_period_wrapper" onClick={periodClick}>
-                    <span></span>
-                    <p className="date">{year}</p>
-                    <p className="title">{title}</p>
-                </div>
-                </>
-            ))
-            
-        )
-        )
-    );
+export default function Period({
+  title,
+  year,
+  isDetail,
+  paragraph,
+  periodClick,
+}: periodProps) {
+  return isDetail ? (
+    <div className="detail_container">
+      <div className="year">{year}</div>
+      <div className="title">{title}</div>
+      <p>{paragraph}</p>
+      <div className="bg"></div>
+    </div>
+  ) : Number(year) ? (
+    <>
+      <div class="period_wrapper" onClick={periodClick}>
+        <span class="left_circle"></span>
+        <p className="date">{year}</p>
+        <span class="right_circle"></span>
+        <p className="title">{title}</p>
+      </div>
+    </>
+  ) : (
+    <>
+      <div class="mini_period_wrapper" onClick={periodClick}>
+        <span></span>
+        <p className="date">{year}</p>
+        <p className="title">{title}</p>
+      </div>
+    </>
+  );
 }
