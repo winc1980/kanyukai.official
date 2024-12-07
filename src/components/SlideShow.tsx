a1b7dbbccf6e4f4c06a878eac15fed3edc0627f6
A1 <tkschr1125@gmail.com>
2024-12-07 20:27:44 +0900

    header

--- src/components/SlideShow.tsx
+++ src/components/SlideShow.tsx
@@ -5,87 +5,104 @@ import "../scss/top.scss";
 import "../scss/slide.scss";
 
 interface Props {
-    imgProps: ImageMetadata[];
+  imgProps: ImageMetadata[];
+  showIndicators?: boolean;
+  showNextPrevButton?: boolean;
 }
 
-export default function SlideComponent({ imgProps }: Props) {
-    const slideLength = imgProps.length;
-    const [slidePos, changeSlidePos] = useState(0);
-    const autoPlayInterval= useRef<number | null>(null);
+export default function SlideComponent({
+  imgProps,
+  showIndicators = true,
+  showNextPrevButton = true,
+}: Props) {
+  const slideLength = imgProps.length;
+  const [slidePos, changeSlidePos] = useState(0);
+  const autoPlayInterval = useRef<number | null>(null);
 
-    function nextClick() {
-        changeSlidePos((prev) => (prev + 1) % slideLength);
-        resetAutoPlayInterval();
+  function nextClick() {
+    changeSlidePos((prev) => (prev + 1) % slideLength);
+    resetAutoPlayInterval();
+  }
+
+  function prevClick() {
+    changeSlidePos((prev) => (prev - 1 + slideLength) % slideLength);
+    resetAutoPlayInterval();
+  }
+
+  function startAutoPlay() {
+    autoPlayInterval.current = window.setInterval(nextClick, 3000);
+  }
+
+  function resetAutoPlayInterval() {
+    if (autoPlayInterval.current) {
+      clearInterval(autoPlayInterval.current);
     }
+    startAutoPlay();
+  }
 
-    function prevClick() {
-        changeSlidePos((prev) => (prev - 1 + slideLength) % slideLength);
-        resetAutoPlayInterval();
-    }
+  useEffect(() => {
+    startAutoPlay();
+    return () => {
+      if (autoPlayInterval.current) {
+        clearInterval(autoPlayInterval.current);
+      }
+    };
+  }, []);
 
-    function startAutoPlay() {
-        autoPlayInterval.current = window.setInterval(nextClick, 3000);
-    }
+  function indicatorClicked(indicatorPos: number) {
+    changeSlidePos(indicatorPos);
+    resetAutoPlayInterval();
+  }
 
-    function resetAutoPlayInterval() {
-        if(autoPlayInterval.current) {
-            clearInterval(autoPlayInterval.current);
-        }
-        startAutoPlay();
-    }
-
-    useEffect(() => {
-        startAutoPlay();
-        return () => {
-            if(autoPlayInterval.current) {
-                clearInterval(autoPlayInterval.current);
-            }
-        };
-    }, []);
-
-    function indicatorClicked(indicatorPos: number) {
-        changeSlidePos(indicatorPos);
-        resetAutoPlayInterval();
-    }
-
-    return (
-        <div class="slide_wrapper">
-            <div>
-                <div class="flex absolute top-3 left-3 z-50">
-                    <div id="prev" class="prev w-10" onClick={prevClick}>
-                        <PrevIcon />
-                    </div>
-                    <div id="next" class="next ml-5 w-10" onClick={nextClick}>
-                        <NextIcon />
-                    </div>
-                </div>
-                
-                <ul class="indicator" id="indicator">
-                    {imgProps.map((_, index) => (
-                        <li
-                            class="list"
-                            style={{
-                                "background-color": `${index === slidePos % slideLength ? "#000" : "#fff"}`,
-                            }}
-                            onClick={() => indicatorClicked(index)}
-                        ></li>
-                    ))}
-                </ul>
-            </div>
-            <div
-                id="slide"
-                style={{
-                    width: `${100 * slideLength}%`,
-                    transform: `translateX(-${(100 / slideLength) * (slidePos % slideLength)}%)`,
-                }}
-            >
-                {imgProps.map((img, index) => (
-                    <div >
-                        <img src={img.src} width={img.width} height={img.height} alt="img" />
-                    </div>
-                ))}
-            </div>
-            
+  return (
+    <div class="slide_wrapper">
+      <div>
+        <div
+          style={{ display: `${showNextPrevButton ? "flex" : "none"}` }}
+          class="flex absolute top-3 left-3 z-50"
+        >
+          <div id="prev" class="prev w-10" onClick={prevClick}>
+            <PrevIcon />
+          </div>
+          <div id="next" class="next ml-5 w-10" onClick={nextClick}>
+            <NextIcon />
+          </div>
         </div>
-    );
+
+        <ul
+          class="indicator"
+          id="indicator"
+          style={{ display: `${showIndicators ? "flex" : "none"}` }}
+        >
+          {imgProps.map((_, index) => (
+            <li
+              class="list"
+              style={{
+                "background-color": `${index === slidePos % slideLength ? "#000" : "#fff"}`,
+              }}
+              onClick={() => indicatorClicked(index)}
+            ></li>
+          ))}
+        </ul>
+      </div>
+      <div
+        id="slide"
+        style={{
+          width: `${100 * slideLength}%`,
+          transform: `translateX(-${(100 / slideLength) * (slidePos % slideLength)}%)`,
+        }}
+      >
+        {imgProps.map((img, index) => (
+          <div>
+            <img
+              src={img.src}
+              width={img.width}
+              height={img.height}
+              alt="img"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
--- src/utils/svgs.tsx
+++ src/utils/svgs.tsx
@@ -1,36 +1,64 @@
 export const BigUnderline = () => {
-    return (
-        <svg
-            width="36"
-            height="9"
-            viewBox="0 0 36 9"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <line x1="0" y1="8.5" x2="36" y2="8.5" stroke="black" />
-            <line
-              x1="35.738"
-              y1="8.42583"
-              x2="22.738"
-              y2="0.425829"
-              stroke="black"
-            />
-          </svg>
-    );
-}
+  return (
+    <svg
+      width="36"
+      height="9"
+      viewBox="0 0 36 9"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <line x1="0" y1="8.5" x2="36" y2="8.5" stroke="black" />
+      <line x1="35.738" y1="8.42583" x2="22.738" y2="0.425829" stroke="black" />
+    </svg>
+  );
+};
 
 export function PrevIcon() {
-    return (
-        <svg width="10" height="12" viewBox="0 0 10 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M9 1L1 6L9 11" stroke="black"/>
-        </svg>
-    );
+  return (
+    <svg
+      width="10"
+      height="12"
+      viewBox="0 0 10 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M9 1L1 6L9 11" stroke="black" />
+    </svg>
+  );
 }
 
 export function NextIcon() {
-    return (
-        <svg width="10" height="12" viewBox="0 0 10 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M1 1L9 6L1 11" stroke="black"/>
-        </svg>
-    );
+  return (
+    <svg
+      width="10"
+      height="12"
+      viewBox="0 0 10 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M1 1L9 6L1 11" stroke="black" />
+    </svg>
+  );
+}
+
+export function UnderBar() {
+  return (
+    <svg
+      width="40"
+      height="3"
+      viewBox="0 0 40 3"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <line
+        x1="1.00049"
+        y1="1.64587"
+        x2="39.0005"
+        y2="1.64587"
+        stroke="black"
+        stroke-width="2"
+        stroke-linecap="round"
+      />
+    </svg>
+  );
 }
