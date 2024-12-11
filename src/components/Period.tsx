d25f362bbd8fbf82deaaf6d4df0b973bde0196c4
A1 <tkschr1125@gmail.com>
2024-12-12 00:37:45 +0900

    information responsive

--- src/components/Period.tsx
+++ src/components/Period.tsx
@@ -15,59 +15,84 @@ export default function Period({
   periodClick,
 }: periodProps) {
   const periodRef = useRef<HTMLDivElement>(null);
+  const [windowWidth, setWindowWidth] = useState(0);
   const [xCoord, setXCoord] = useState(0);
+  const [yCoord, setYCoord] = useState(0);
+
+  useEffect(() => {
+    setWindowWidth(window.innerWidth);
+
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   useEffect(() => {
     const period = periodRef.current;
-    if(!period) return;
+    if (!period) return;
 
-    const updateXCoord = () => {
+    const updateCoord = () => {
       const rect = period.getBoundingClientRect();
-      setXCoord(rect.left);
-    };
+      if(windowWidth <= 767){
+        setYCoord(rect.top);
+      }else {
+        setXCoord(rect.left);
+      }
+    }
 
     const parent = period.parentElement;
-    if(parent) {
-      parent.addEventListener("scroll", updateXCoord);
+    if (parent) {
+      if (windowWidth <= 767) {
+        parent.addEventListener("scroll", updateCoord);
+      } else {
+        parent.addEventListener("scroll", updateCoord);
+      }
     }
 
-    const observer = new ResizeObserver(updateXCoord);
+    const observer = new ResizeObserver(updateCoord,);
     observer.observe(period);
 
-    updateXCoord();
+    updateCoord();
 
     return () => {
-      if(parent) {
-        parent.removeEventListener("scroll", updateXCoord);
+      if (parent) {
+        updateCoord();
       }
       observer.disconnect();
-    }
+    };
   }, []);
 
+  console.log(yCoord);
+  console.log(windowWidth);
+
   return (
-    <div ref={(el) => {
+    <div
+    ref={(el) => {
       periodRef.current = el;
-    }}>
-      {xCoord < 400 && xCoord >50 ? (
-        <div class="detail_container" >
-          <div class="year">{year}</div>
-          <div class="title">{title}</div>
+    }}
+      id="period"
+      class="p-period"
+    >
+      {(windowWidth > 767 && xCoord < 400 && xCoord > 50) ||
+      (windowWidth <= 767 && (yCoord < 1150 && yCoord > 1050)) ? (
+        <div class="p-period__detail">
+          <h3 class="p-period__detail__year">{year}</h3>
+          <h4 class="p-period__detail__title">{title}</h4>
           <p>{paragraph}</p>
-          <div class="bg"></div>
         </div>
       ) : (
-        <>
-          <div class="period_wrapper" onClick={periodClick}>
-            <span class="left_circle"></span>
-            <p class="date">{year}</p>
-            <span class="right_circle"></span>
-            <p class="title">{title}</p>
-          </div>
-        </>
-      ) 
-    }
-
+        <div class="period_wrapper" onClick={periodClick}>
+          <span class="left_circle"></span>
+          <p class="date">{year}</p>
+          <span class="right_circle"></span>
+          <p class="title">{title}</p>
+        </div>
+      )}
     </div>
-  )
-  
+  );
 }
+
