5fef1a0a4432e2fafc6f89a2ad44a48213ff7742
tanakamoe <m2216050tm@ug.jwu.ac.jp>
2024-12-20 14:27:00 +0900

    cms化

--- src/lib/microcms.ts
+++ src/lib/microcms.ts
@@ -2,10 +2,15 @@
 import type { MicroCMSQueries, MicroCMSListContent } from "microcms-js-sdk";
 import { createClient } from "microcms-js-sdk";
 
-const client = createClient({
+const client1 = createClient({
   serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
   apiKey: import.meta.env.MICROCMS_API_KEY,
-});
+});//client1に含まれる情報：activities,awards,history
+
+const client2 = createClient({
+  serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN2,
+  apiKey: import.meta.env.MICROCMS_API_KEY2,
+});//client1に含まれる情報：invitation
 
 // Awards型定義
 export type Awards = {
@@ -27,15 +32,80 @@ export type Awards = {
   
   //APIの呼び出し
   export const getAwards = async (queries?: MicroCMSQueries) => {
-    return await client.get<AwardsResponse>({ endpoint: "awards", queries });
+    return await client1.get<AwardsResponse>({ endpoint: "awards", queries });
   };
   export const getAwardsDetail = async (
     contentId: string,
     queries?: MicroCMSQueries
   ) => {
-    return await client.getListDetail<Awards>({
+    return await client1.getListDetail<Awards>({
       endpoint: "awards",
       contentId,
       queries,
     });
   };
+
+
+  // Activities型定義
+export type Activities= {
+  id: string;
+  date: string;
+  title: string;
+  desicription:string;
+  og_Image?: {
+    url: string;
+  };
+};
+export type ActivitiesResponse = {
+  totalCount: number;
+  offset: number;
+  limit: number;
+  contents: Activities[];
+};
+
+//APIの呼び出し
+export const getActivities = async (queries?: MicroCMSQueries) => {
+  return await client1.get<ActivitiesResponse>({ endpoint: "activities", queries });
+};
+export const getActivitiesDetail = async (
+  contentId: string,
+  queries?: MicroCMSQueries
+) => {
+  return await client1.getListDetail<Activities>({
+    endpoint: "activities",
+    contentId,
+    queries,
+  });
+};
+
+
+ // Invitation型定義
+ export type Invitation= {
+  id: string;
+  date: string;
+  title: string;
+  description:string;
+};
+export type InvitationResponse = {
+  totalCount: number;
+  offset: number;
+  limit: number;
+  contents: Invitation[];
+};
+
+//APIの呼び出し
+export const getInvitation = async (queries?: MicroCMSQueries) => {
+  return await client2.get<InvitationResponse>({ endpoint: "invitation", queries });
+};
+export const getInvitationDetail = async (
+  contentId: string,
+  queries?: MicroCMSQueries
+) => {
+  return await client2.getListDetail<Invitation>({
+    endpoint: "invitation",
+    contentId,
+    queries,
+  });
+};
+
+
