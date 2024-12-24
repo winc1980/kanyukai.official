14fdabb05dc1b33df6f4486170de7d957807ea46
tytdroid <k86843347@gmail.com>
2024-12-24 18:41:06 +0900

    slidecomponent history修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
 import solidJs from "@astrojs/solid-js";
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
