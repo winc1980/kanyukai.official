80270c4917d39ac6b51126964db9e5f71fc02889
shotaro <okita0510sb@i.softbank.jpgit config --global user.email okita0510sb@i.softbank.jp>
2025-01-14 02:38:46 +0900

    debug

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from "@astrojs/react";
 
 export default defineConfig({
   site: 'https://kanyukai.github.io',
-  base: '/kanyukai.official',
+  // base: '/kanyukai.official',
   vite: {
     resolve: {
       alias: {
