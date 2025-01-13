79727ac623cb44e947be56c95e8253b958f5b863
shotaro <okita0510sb@i.softbank.jpgit config --global user.email okita0510sb@i.softbank.jp>
2025-01-14 02:10:52 +0900

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
 import solidJs from "@astrojs/solid-js";
-
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://kanyukai.github.io',
+  base: '/kanyukai.official',
   vite: {
     resolve: {
       alias: {
