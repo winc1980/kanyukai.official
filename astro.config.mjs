598819f12e920d977c37606d86a7951254ee8fb2
shotaro <okita0510sb@i.softbank.jpgit config --global user.email okita0510sb@i.softbank.jp>
2025-01-14 01:19:54 +0900

    debug

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,27 +2,40 @@
 // import tailwind from "@astrojs/tailwind";
 // import preact from "@astrojs/preact";
 // import solidJs from "@astrojs/solid-js";
-// import { fileURLToPath } from "url";
-// import path from "path";
+
+
+// export default defineConfig({
+//   site: 'https://kanyukai.github.io',
+//   base: '/kanyukai.official',
+//   vite: {
+//     resolve: {
+//       alias: {
+//         "@": "/src",
+//       },
+//     },
+//   },
+//   build: {
+//     format: "preserve", // NOTE: 出力ファイルの設定
+//   },
+//   integrations: [tailwind(), preact(), solidJs()]
+// });
+
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
 import solidJs from "@astrojs/solid-js";
+import { fileURLToPath } from "url";
+import path from "path";
 
-
-// https://astro.build/config
 export default defineConfig({
   site: 'https://kanyukai.github.io',
   base: '/kanyukai.official',
   vite: {
     resolve: {
       alias: {
-        "@": "/src",
+        "@": path.resolve(fileURLToPath(import.meta.url), "./src"), // 正しいエイリアス解決
       },
     },
   },
-  build: {
-    format: "preserve", // NOTE: 出力ファイルの設定
-  },
-  integrations: [tailwind(), preact(), solidJs()]
+  integrations: [tailwind(), preact(), solidJs()],
 });
