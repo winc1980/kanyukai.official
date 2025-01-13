5724801130b20cbe76842aef91a8ae96203579d6
shotaro <okita0510sb@i.softbank.jpgit config --global user.email okita0510sb@i.softbank.jp>
2025-01-14 01:23:52 +0900

    debug

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,8 @@
-// import { defineConfig } from "astro/config";
-// import tailwind from "@astrojs/tailwind";
-// import preact from "@astrojs/preact";
-// import solidJs from "@astrojs/solid-js";
-
-
-// export default defineConfig({
-//   site: 'https://kanyukai.github.io',
-//   base: '/kanyukai.official',
-//   vite: {
-//     resolve: {
-//       alias: {
-//         "@": "/src",
-//       },
-//     },
-//   },
-//   build: {
-//     format: "preserve", // NOTE: 出力ファイルの設定
-//   },
-//   integrations: [tailwind(), preact(), solidJs()]
-// });
-
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
 import solidJs from "@astrojs/solid-js";
-import { fileURLToPath } from "url";
-import path from "path";
+
 
 export default defineConfig({
   site: 'https://kanyukai.github.io',
@@ -33,9 +10,32 @@ export default defineConfig({
   vite: {
     resolve: {
       alias: {
-        "@": path.resolve(fileURLToPath(import.meta.url), "/src"), // 正しいエイリアス解決
+        "@": "/src",
       },
     },
   },
-  integrations: [tailwind(), preact(), solidJs()],
+  build: {
+    format: "preserve", // NOTE: 出力ファイルの設定
+  },
+  integrations: [tailwind(), preact(), solidJs()]
 });
+
+// import { defineConfig } from "astro/config";
+// import tailwind from "@astrojs/tailwind";
+// import preact from "@astrojs/preact";
+// import solidJs from "@astrojs/solid-js";
+// import { fileURLToPath } from "url";
+// import path from "path";
+
+// export default defineConfig({
+//   site: 'https://kanyukai.github.io',
+//   base: '/kanyukai.official',
+//   vite: {
+//     resolve: {
+//       alias: {
+//         "@": path.resolve(fileURLToPath(import.meta.url), "/src"), // 正しいエイリアス解決
+//       },
+//     },
+//   },
+//   integrations: [tailwind(), preact(), solidJs()],
+// });
