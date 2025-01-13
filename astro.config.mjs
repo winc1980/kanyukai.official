74bb9866ced3be3857ec3c2aacb79081baea9a0b
shotaro <okita0510sb@i.softbank.jpgit config --global user.email okita0510sb@i.softbank.jp>
2025-01-14 01:07:33 +0900

    使用フレームワークの訂正

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,27 @@
+// import { defineConfig } from "astro/config";
+// import tailwind from "@astrojs/tailwind";
+// import preact from "@astrojs/preact";
+// import solidJs from "@astrojs/solid-js";
+// import { fileURLToPath } from "url";
+// import path from "path";
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
 import solidJs from "@astrojs/solid-js";
-import { fileURLToPath } from "url";
-import path from "path";
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://kanyukai.github.io',
   base: '/kanyukai.official',
   vite: {
     resolve: {
       alias: {
-        "@": path.resolve(fileURLToPath(import.meta.url), "./src"), // 安全にパスを解決
+        "@": "/src",
       },
     },
   },
-  integrations: [tailwind(), preact(), solidJs()],
+  build: {
+    format: "preserve", // NOTE: 出力ファイルの設定
+  },
+  integrations: [tailwind(), preact(), solidJs()]
 });
