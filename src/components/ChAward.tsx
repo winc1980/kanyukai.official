27e83efb7a6791a7e8168a55d4931e8d6d23b972
shotaro <okita0510sb@i.softbank.jpgit config --global user.email okita0510sb@i.softbank.jp>
2025-02-15 14:59:04 +0900

    medalcolarの準備

--- src/components/ChAward.tsx
+++ src/components/ChAward.tsx
@@ -2,26 +2,12 @@ import "../scss/chAward.scss";
 import type { Award } from "../pages/awards.astro";
 import SlideComponent from "./SlideShow";
 
-const medalColorClass = {
-  金: "bg-yellow-500",
-  銀: "bg-gray-400",
-  銅: "bg-orange-600",
-  その他: "bg-blue-300",
-};
-
-// medalcolor の型を考慮して処理
-const getMedalColor = (medalcolor: string | string[] | undefined) => {
-  if (!medalcolor) return medalColorClass["その他"]; // undefinedの場合のデフォルト
-  const color = Array.isArray(medalcolor) ? medalcolor[0] : medalcolor; // 配列なら最初の要素を取得
-  return medalColorClass[color] || medalColorClass["その他"];
-};
-
-export default function ChAward({ date, tags, title, content, images, medalcolor }: Award & { medalcolor: string | string[] }) {
+export default function ChAward({ date, tags, title, content, images }: Award) {
   return (
     <div class="ch__award">
       <div class="ch__award__top">
         <div class="ch__award__top__left">
-          <span class={`medalspan ${getMedalColor(medalcolor)}`}></span>
+          <span></span>
           <div>
             <h5 class="ch__award__top__left__date zenkaku_light">{date}</h5>
             <h4 class="ch__award__top__left__title zenkaku_normal">{title}</h4>
